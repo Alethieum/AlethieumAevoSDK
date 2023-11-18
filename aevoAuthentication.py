214b94e3159d77505015ecfa629b44a717207094
Carbon-JRC <81396161+Carbon-JRC@users.noreply.github.com>
2023-11-18 18:26:36 +0000

    delete

--- aevoAuthentication.py
+++
@@ -1,9 +0,0 @@
-from AlethieumAevoSDK import AevoClient
-from clientConfig import clientConfig
-from loguru import logger as log
-
-aevo = AevoClient(**clientConfig)
-
-
-transaction = aevo.rest_get_apikey()
-log.info(f"API Key: {transaction}")
