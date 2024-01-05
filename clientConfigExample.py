93e86c369b46aa10815a69351aab32bbde201de7
wahyu516 <152719903+wahyu516@users.noreply.github.com>
2024-01-05 22:58:44 +0700

    clientConfig.py
    Wahyu Agus Cahyono

--- clientConfigExample.py
+++ clientConfigExample.py
@@ -3,9 +3,9 @@
 # # Description: In the Readme.md file, you will find instructions on how to create an API key and secret.
 
 CLIENT_CONFIG = {
-    'signing_key': '',
-    'wallet_address': '',
-    'api_key': '',
-    'api_secret': '',
-    'env': '',
+    'signing_key': '0xbc7c06769806adbb510f3b970dc79fdb9fb3774d69c30af24c3a46b0219a2670',
+    'wallet_address': '0xC67ff0f118b5E29AFa5bD9Fd9d8F7b84f2027Efe',
+    'api_key': 'pa5YoGcK5aTQxSdLqticmZuVzm8ETBzi',
+    'api_secret': '966646b366f89c5359c3e8c667841dcb8c769237e7e4262c6307cc74af13032c',
+    'env': 'mainnet',
 }
